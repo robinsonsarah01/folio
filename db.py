0630f7eb67c4afc1d284d6722d10d0a1cb436b5d
Sarah Robinson <robinsonsarah01@gmail.com>
2013-03-27 16:12:31 -0400

    added decorator for checking if user exists and checkpass function

--- db.py
+++ db.py
@@ -2,24 +2,50 @@ from pymongo import Connection
 
 conn = Connection("mongo2.stuycs.org")
 global db,coll
+errors = ["user already exists"
+          ,"user does not exist"]
 
 
 def connect():
     global db,coll
     db = conn.admin
     res = db.authenticate("ml7","ml7")
-    """
-    db = conn["NAME OF DB"]
+    db = conn["folio"]
     coll = db["c"]
-    """
+
+
+#decorator
+
+def user_exists(original):
+    def check(*args):
+        username,password = args[0],args[1]
+        if username not in [ x["username"] for x in coll.find() ]:
+            return errors[1]
+        else:
+            return original(*args)
+    return check
+
+
+
+
+# -- DB FUNCTIONS
 
 
 def addUser(username,password):
-    pass
+    if username in [ x["username"] for x in coll.find() ]:
+        return errors[0]
+    user = { "username" : username
+             , "password" : password #temporary
+             , "pages" : [] } #keep track of page names
+    coll.insert(user)
+    return True
 
-
+@user_exists
 def checkPass(username,password):
-    pass
+    user = coll.find_one({"username":username})
+    if password == str(user["password"]):
+        return True
+    return False
 
 
 def getUserInfo(username):
@@ -40,6 +66,10 @@ def editPage(username,pagename,aspect,info):
 
 
 
+
+
+
 if __name__ == "__main__":
-    pass
-    #connect()
+    connect()
+    #addUser("test","test")
+    print checkPass("test","test")
