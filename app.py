acde9b2b313163258ab25354acf1fe268f69bc88
Sarah Robinson <robinsonsarah01@gmail.com>
2013-03-28 13:54:03 -0400

    added home and page to app.py for fleshing out later

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-from flask import redirect
+from flask import Flask,request,render_template,redirect
+import db
 
 app = Flask(__name__)
 
 
-@app.route("/", methods = ['get','post'])
-def home():
+@app.route("/", methods = ['GET','POST'])
+def login():
     if request.method=='GET':
         return render_template("login.html")
     else:
          button = request.form['password']
          print(button)
-         return render_template("login.html")
+         return "PLACEHOLDER - YOU HAVE BEEN LOGGED IN"
 
 
+@app.route("/<username>",methods = ["GET","POST"])
+def home(username=""):
+    if not username:
+        return redirect(url_for("login"))
+    else:
+        return "PLACEHOLDER - HOME PAGE FOR " + username
 
-# decorator reminder:
-# https://github.com/stuycs-ml7/classcode/blob/master/decorators/login2.py
+
+@app.route("/<username>/<page>",methods = ["GET","POST"])
+def folio(username="",page=""):
+    if not username:
+        return redirect(url_for("login"))
+    if not page:
+        return redirect(url_for("home"))
+    else:
+        return "PLACEHOLDER - FOLIO PAGE FOR " + username + "'s " + page
+
 
 if __name__ == "__main__":
     
