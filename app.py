5ca64b4f5f0e61b360f3781fba24bc4a6d3d45f9
Sarah Robinson <robinsonsarah01@gmail.com>
2013-04-02 18:04:45 -0400

    working on app.py and login stuff now that forms are in login -- NOT done

--- app.py
+++ app.py
@@ -2,22 +2,28 @@ from flask import Flask,request,render_template,redirect, session, url_for
 import db
 
 app = Flask(__name__)
-
+Flask.secret_key = "folio is short for portfolio" #obvs temporary
 
 @app.route("/", methods = ['GET','POST'])
 def login():
     if request.method=='GET':
         return render_template("login.html")
-    else:
-        try:
-            button = request.form['password']
-            print(button)
+    else: 
+        button = request.form["button"]
+        if button == "Login":
+            password = request.form["password"]
+            email = request.form["login"]
+            #check pass should be done with js and ajax
+            #PLACEHOLDER - ADD USER TO SESSION / LOG THEM IN
             return "PLACEHOLDER - YOU HAVE BEEN LOGGED IN"
-        except:
+        
+        """
             name = str(request.form['first_name']) + str(request.form['last_name'])
             email = str(request.form['new_email'])
             password = str(request.form['new_password'])
             return "PLACEHOLDER - YOU HAVE BEEN REGISTERED"
+        """
+        return "doot doot"
 
 @app.route("/<username>",methods = ["GET","POST"])
 def home(username=""):
@@ -38,6 +44,6 @@ def folio(username="",page=""):
 
 
 if __name__ == "__main__":
-    
+    db.connect()
     app.run(debug=True)
 
