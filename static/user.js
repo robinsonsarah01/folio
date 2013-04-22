de4a3f53c91c183a9cd7d0e3988bd078b99f8072
Sarah Robinson <robinsonsarah01@gmail.com>
2013-04-21 20:23:14 -0400

    user.html,user.js updated - page now loaded thru js

--- static/user.js
+++ static/user.js
@@ -1,27 +1,39 @@
 var username
 var currpage
-var nextpage
+//var nextpage
 
 
 function getInfo(){
-    var url = document.URL
-    var info = url.split("/")
-    username = info[3]
-    currpage = info[4]
-    return [ username, currpage ]
+    var url = document.URL;
+    var info = url.split("/");
+    username = info[3];
+    currpage = info[4];
+    return [ username, currpage ];
 }
 
 function getPage(page){
     $.getJSON("/getPage",{"username":username,"page":page},function(data){
-	nextpage = data;
+	loadFolioData(data);
     });
 }
     
 
+function loadFolioData(pagedata){
+    //can do things to pagedata to make it pretty before this step
 
+    $("#folio_contents").append("<p>"+pagedata+"</p>");
+}
+
+
+function viewFolio(page){
+    getPage(page); //gets data and loads it
+    console.log(window.location);
+
+}
 
 
 $(document).ready( function() {
     //console.log(document.URL); //also window.location
-    getInfo()
+    getInfo();
+    viewFolio(currpage);
 });
