1db84d27f450ff170364a1d5d06bcf56b1a3e74f
Sarah Robinson <robinsonsarah01@gmail.com>
2013-04-24 20:49:36 -0400

    switching between about and page works in setup.html

--- static/setup.js
+++ static/setup.js
@@ -7,19 +7,25 @@ function getInfo() {
     return username
 }
 
-function loadFolioData(data){
+function loadFolioData(data,page){
     //can do things to pagedata to make it pretty before this step
     //like mess with it in js and use different methods to display it etc
     
-    console.log("in loadFolioData");
-    $("#contents").empty().append("<p>"+data+"</p>");
+    //console.log("in loadFolioData");
+    $("#about_me").remove();
+    $("#contents").empty().append("<p id='data'>"+data+"</p>");
+
+    if (page == "about"){
+	$("#contents").prepend('<div id="about_me"></div>');
+	$("#about_me").append('<center> <img src="/static/shan.png"></center><br><br> <form> <textarea type="text" id="blurb" resize="false" placeholder="Write about yourself here."></textarea></form>');
+    }
 }
 
 function getPage(page){
-    console.log("in getPage: "+page);
+    //console.log("in getPage: "+page);
     $.getJSON("/getPage",{"username":username,"page":page},function(data){
-	console.log("in getPage json call");
-	loadFolioData(data);
+	//console.log("in getPage json call");
+	loadFolioData(data,page);
     });
 }
 
@@ -40,6 +46,8 @@ function createFolio() {
 
 $(document).ready( function() {
     $("#add_folio").click(createFolio);
+
+    // $("#blurb_button_save").click(saveBlurb);
 
     //css is all peter, thanks peter
     $('.left_rectangle').click(function(){
