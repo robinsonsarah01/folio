cf92a52f547ebe310d38fa3055c7beacaf0b6f47
kingofirony <kingofirony@gmail.com>
2013-04-29 12:30:10 -0400

    Appends to the html a create new project page.

--- static/setup.js
+++ static/setup.js
@@ -150,12 +150,54 @@ function addFolio() { //actually goes to server
 	      });
 }
 
+//create project
+
+function createProject() {
+    $("#about_me").remove()
+    $("#contents").empty().append("<div id='add'><h1>Add a new Project!</h1><br><textarea id='add_title' type='text' resize='false' placeholder='Title'></textarea><div id='title_note'><b>The title is case-sensitive, and no spaces, please!</b></div><br><textarea id='add_description' type='text' resize='false' placeholder='Write about your project here'></textarea><br><textarea id='add_link' type='text' resize='false' placeholder='Put a link to the project here(If Available)'></textarea><br><textarea id='add_embed' type='text' resize='false' placeholder='Put any embedded content code here'></textarea><button type='button' id='create' name='Create'>Create</button></div>");
+
+    $("#create").click(addFolio);
+}
+
+function addProject() { //actually goes to server
+    $("#create").attr("disabled","disabled");
+
+    name = $("#add_title").val();
+    des = $("#add_description").val();
+
+    if (! /^[a-zA-Z0-9]+$/.test(name) ){
+	$("#contents").append("<p id='add_error'>Alphanumeric characters only, and no spaces, please.</p>");
+	$("#add_error").fadeOut(2500,function(){
+	    
+	    $("#add_title").val("");
+	    $("#create").removeAttr("disabled");
+	    $("#add_error").remove();
+	});
+	return false; //break function
+    }
+
+    //info = { "description" : description, "projects" : [] };
+
+    $.getJSON("/addPage",{"username":username,"pagename":name,"description":des},
+	      function(data){
+		  if (data == true){
+		      window.location.reload(true);
+		  }
+		  else {
+		      console.log(data);
+		      $("#contents").append("<p id='add_error'>Something went wrong. Please try again.</p>");
+		      $("#create").removeAttr("disabled");
+		  }
+
+	      });
+}
+
 
 // startup
 
 $(document).ready( function() {
     $("#add_folio").click(createFolio);
-
+    $("#newProj").click(createProject);
     //$("#blurb_save").click(saveBlurb);
 
     //css is all/mostly peter, thanks peter
